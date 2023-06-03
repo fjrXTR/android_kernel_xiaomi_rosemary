flashokiller/kernel_xiaomi_cannon

drivers: mtk-wifi: gen4m: Use wifi_fw_entry pointer
* fw_entry pointer is already used in touchscreen and causes conflict during linking

Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -171,7 +171,7 @@ void *wlan_fb_notifier_priv_data;
 #define  KGIDT_VALUE(v) v
 #endif
 
-const struct firmware *fw_entry;
+const struct firmware *wifi_fw_entry;
 
 /* Default */
 static uint8_t *apucFwName[] = {
@@ -256,7 +256,7 @@ uint32_t kalFirmwareOpen(IN struct GLUE_INFO *prGlueInfo,
 		 *               "/firmware/image"
 		 * Linux path: "/lib/firmware", "/lib/firmware/update"
 		 */
-		ret = request_firmware(&fw_entry, apucNameTable[ucNameIdx],
+		ret = request_firmware(&wifi_fw_entry, apucNameTable[ucNameIdx],
 				       prGlueInfo->prDev);
 
 		if (ret) {
@@ -302,7 +302,7 @@ error_open:
 /*----------------------------------------------------------------------------*/
 uint32_t kalFirmwareClose(IN struct GLUE_INFO *prGlueInfo)
 {
-	release_firmware(fw_entry);
+	release_firmware(wifi_fw_entry);
 
 	return WLAN_STATUS_SUCCESS;
 }
@@ -327,12 +327,12 @@ uint32_t kalFirmwareLoad(IN struct GLUE_INFO *prGlueInfo,
 	ASSERT(pu4Size);
 	ASSERT(prBuf);
 
-	if ((fw_entry == NULL) || (fw_entry->size == 0)
-	    || (fw_entry->data == NULL)) {
+	if ((wifi_fw_entry == NULL) || (wifi_fw_entry->size == 0)
+	    || (wifi_fw_entry->data == NULL)) {
 		goto error_read;
 	} else {
-		memcpy(prBuf, fw_entry->data, fw_entry->size);
-		*pu4Size = fw_entry->size;
+		memcpy(prBuf, wifi_fw_entry->data, wifi_fw_entry->size);
+		*pu4Size = wifi_fw_entry->size;
 	}
 
 	return WLAN_STATUS_SUCCESS;
@@ -360,7 +360,7 @@ uint32_t kalFirmwareSize(IN struct GLUE_INFO *prGlueInfo,
 	ASSERT(prGlueInfo);
 	ASSERT(pu4Size);
 
-	*pu4Size = fw_entry->size;
+	*pu4Size = wifi_fw_entry->size;
 
 	return WLAN_STATUS_SUCCESS;
 }

fjrXTR/android_kernel_xiaomi_rosemary

Signed-off-by: Momo Belia Deviluke <fajarslebew31@gmail.com>

drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -180,7 +180,7 @@ static uint32_t kalPerMonUpdate(IN struct ADAPTER *prAdapter);
 #define  KGIDT_VALUE(v) v
 #endif
 
-const struct firmware *fw_entry;
+const struct firmware *wifi_fw_entry;
 
 /* Default */
 static uint8_t *apucFwName[] = {
@@ -265,7 +265,7 @@ uint32_t kalFirmwareOpen(IN struct GLUE_INFO *prGlueInfo,
 		 *               "/firmware/image"
 		 * Linux path: "/lib/firmware", "/lib/firmware/update"
 		 */
-		ret = _kalRequestFirmware(&fw_entry, apucNameTable[ucNameIdx],
+		ret = _kalRequestFirmware(&wifi_fw_entry, apucNameTable[ucNameIdx],
 				       prGlueInfo->prDev);
 
 		if (ret) {
@@ -311,7 +311,7 @@ error_open:
 /*----------------------------------------------------------------------------*/
 uint32_t kalFirmwareClose(IN struct GLUE_INFO *prGlueInfo)
 {
-	release_firmware(fw_entry);
+	release_firmware(wifi_fw_entry);
 
 	return WLAN_STATUS_SUCCESS;
 }
@@ -336,12 +336,12 @@ uint32_t kalFirmwareLoad(IN struct GLUE_INFO *prGlueInfo,
 	ASSERT(pu4Size);
 	ASSERT(prBuf);
 
-	if ((fw_entry == NULL) || (fw_entry->size == 0)
-	    || (fw_entry->data == NULL)) {
+	if ((wifi_fw_entry == NULL) || (wifi_fw_entry->size == 0)
+	    || (wifi_fw_entry->data == NULL)) {
 		goto error_read;
 	} else {
-		memcpy(prBuf, fw_entry->data, fw_entry->size);
-		*pu4Size = fw_entry->size;
+		memcpy(prBuf, wifi_fw_entry->data, wifi_fw_entry->size);
+		*pu4Size = wifi_fw_entry->size;
 	}
 
 	return WLAN_STATUS_SUCCESS;
@@ -369,7 +369,7 @@ uint32_t kalFirmwareSize(IN struct GLUE_INFO *prGlueInfo,
 	ASSERT(prGlueInfo);
 	ASSERT(pu4Size);
 
-	*pu4Size = fw_entry->size;
+	*pu4Size = wifi_fw_entry->size;
 
 	return WLAN_STATUS_SUCCESS;
 }